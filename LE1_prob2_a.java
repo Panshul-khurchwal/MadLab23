1ad863cc87f149dfccaf7f9e318fe69c1fa1de21
Panshul-khurchwal <211b202@juetguna.in>
2023-05-18 08:48:31 +0530

    Added Files

---
+++ LE1_prob2_a.java
@@ -0,0 +1,23 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int a = 4 ;
+		for(int i = 1 ; i <= 4 ; i++){
+		    for(int j = 4 ; j>i ; j--){
+		        
+		        System.out.print(" ");
+		        
+		        }
+		    for(int k =1 ; k <= i ; k++){
+		          
+		          System.out.print("*");
+		          System.out.print(" ");  
+		    }
+	
+		
+		    System.out.println("");
+		}
+		        
+		
+	}
+}
---
+++ LE1_prob2_b.java
@@ -0,0 +1,21 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int a = 4 ;
+		for(int i = 1 ; i <= 4 ; i++){
+		    for(int k =1 ; k< i ; k++ ){
+		        
+		        System.out.print(" ");
+		      }
+		    for(int y=a+1-i ; y>0; y-- ){
+		      System.out.print("*");  
+		      System.out.print(" ");   
+		    }
+	
+		
+		    System.out.println("");
+		}
+		        
+		
+	}
+}
